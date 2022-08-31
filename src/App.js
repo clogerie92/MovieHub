d5be4f3888f137bf6723227a0cc8fe725977118c
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-31 12:38:42 -0400

    created folder structure for app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>OMDB API</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/API.js
@@ -0,0 +1,3 @@
+const apiKey = "d04ce205";
+const url = "http://www.omdbapi.com/?apikey=d04ce205";
+
